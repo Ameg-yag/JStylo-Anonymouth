1edce3cbbbfa7df09761053c0eaf77fe5df23961
Andrew McDonald <andrewm921@gmail.com>
2013-05-20 11:46:42 -0400

    quick clarification to note added.

--- src/edu/drexel/psal/anonymouth/engine/FeatureSwapper.java
+++ src/edu/drexel/psal/anonymouth/engine/FeatureSwapper.java
@@ -12,7 +12,7 @@ public class FeatureSwapper {
 	/*
 	 * need to get the cluster groups, 
 	 * find the corresponding features in Weka's attributes for the document to anonymize,
-	 * create new Instance objects for each cluster group,
+	 * create new weka Instance objects for the each cluster group,
 	 * and replace the original feature values with the target values from each cluster group.
 	 * 
 	 * Then, we take the trained classifier, and test each of the new instances against it. 
